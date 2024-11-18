d52ee6c4c258f2c5a863c709486900e7a3073d30
Federico Sosa <federico.sosa4848@gmail.com>
2024-11-18 14:54:07 -0300

    ...

--- portfolio-web/src/app/Components/proyect/card/card.component.ts
+++ portfolio-web/src/app/Components/proyect/card/card.component.ts
@@ -11,18 +11,17 @@ import { Component } from '@angular/core';
 export class CardComponent {
   // Arreglo de proyectos con algún ejemplo de datos
   projects = [
-    { title: 'Proyecto 1', description: 'Descripción del proyecto 1' , background: 'https://ipaginaweb.com/wp-content/uploads/angular-1.jpg'},
-    { title: 'Proyecto 2', description: 'Descripción del proyecto 2' },
-    { title: 'Proyecto 3', description: 'Descripción del proyecto 3' },
-    { title: 'Proyecto 4', description: 'Descripción del proyecto 4' },
-    { title: 'Proyecto 5', description: 'Descripción del proyecto 5' },
-    { title: 'Proyecto 6', description: 'Descripción del proyecto 6' },
-    { title: 'Proyecto 7', description: 'Descripción del proyecto 7' },
-    { title: 'Proyecto 8', description: 'Descripción del proyecto 8' },
-    { title: 'Proyecto 9', description: 'Descripción del proyecto 9' },
-    { title: 'Proyecto 10', description: 'Descripción del proyecto 10' },
-    { title: 'Proyecto 11', description: 'Descripción del proyecto 11' }
-  ];
+    { title: 'Nutrit-Track', description: 'Una aplicación nutricional que ayuda a los usuarios a llevar un control detallado de sus comidas diarias y su ingesta calórica.', background: 'captura-nutrit.png' },
+    { title: 'CarOnPoint', description: 'Un sistema de venta de autos usados que permite a los usuarios interactuar mediante un chat para facilitar la compra y venta de vehículos.', background: 'captura-caronp.png' },
+    { title: 'Nutrit-Track Backend', description: 'La versión mejorada de Nutrit-Track, con un backend real desarrollado en Node.js y Express para gestionar datos y usuarios.', background: 'captura-nutri2.png' },
+    { title: 'Biblioteca de Gestión de Libros', description: 'Un sistema de gestión para bibliotecas, que permite el control de préstamos y la administración de los libros disponibles.', background: 'cap-biblio-java.png' },
+    { title: 'Biblioteca C', description: 'Un sistema similar al anterior, pero desarrollado en C con estructuras dinámicas para el manejo de los libros y los usuarios.', background: 'cap-biblio-c.png' },
+    { title: 'Proyectos Académicos en C', description: 'Proyectos académicos realizados en C, abarcando diversos algoritmos y estructuras de datos en este lenguaje.', background: 'proyectsC.png' },
+    { title: 'Proyectos Académicos en JavaScript', description: 'Implementación de proyectos académicos utilizando JavaScript, aplicando conocimientos en programación web y lógica.', background: 'proyectsJavascript.png' },
+    { title: 'Proyectos Académicos en Java', description: 'Proyectos desarrollados en Java, donde se abordan diferentes estructuras de datos y técnicas de programación orientada a objetos.', background: 'proyectsJava.png' },
+    { title: 'Proyectos en Angular', description: 'Desarrollo de proyectos utilizando Angular, enfocándose en la creación de aplicaciones web dinámicas y escalables.', background: 'proyectsAngular.png' },
+];
+
 
   currentIndex: number = 0;
 
---
+++ portfolio-web/src/app/Components/tech-progress/tech-progress.component.ts
@@ -0,0 +1,49 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
+
+@Component({
+  selector: 'app-tech-progress',
+  standalone: true,
+  imports: [CanvasJSAngularChartsModule],
+  templateUrl: './tech-progress.component.html',
+  styleUrls: ['./tech-progress.component.css']
+})
+export class TechProgressComponent {
+  @Input() progress: number = 0; // Progreso recibido del skill seleccionado
+  @Output() progressSelected: EventEmitter<number> = new EventEmitter<number>(); // Emite el progreso seleccionado
+
+  // Configuración del gráfico, con valores dinámicos de progreso y faltante
+  chartOptions = {
+    animationEnabled: true,
+    title: {
+      text: "Progreso"
+    },
+    data: [{
+      type: "doughnut",
+      yValueFormatString: "#,###.##'%'",
+      indexLabel: "{name}",
+      dataPoints: [
+        { y: 0, name: "Realizado" }, // Se actualizará con el valor del progreso
+        { y: 0, name: "Faltante" }, // Se actualizará con el valor faltante
+      ]
+    }]
+  };
+
+  // Método que actualiza el gráfico con el progreso y el valor faltante
+  updateChart() {
+    const realizado = this.progress; // El progreso recibido
+    const faltante = 100 - realizado; // El valor faltante hasta 100%
+    
+    // Actualizamos los puntos de datos en el gráfico
+    this.chartOptions.data[0].dataPoints[0].y = realizado;
+    this.chartOptions.data[0].dataPoints[1].y = faltante;
+
+    // Emitimos el progreso al componente padre (si lo necesitas)
+    this.progressSelected.emit(this.progress);
+  }
+
+  // Llamamos a updateChart cuando se reciba el valor de progreso
+  ngOnChanges() {
+    this.updateChart();
+  }
+}
