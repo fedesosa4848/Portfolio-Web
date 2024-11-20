d8768a4de6c7f8d7f3f783d7bb8ab42a8f5b007c
Federico Sosa <federico.sosa4848@gmail.com>
2024-11-20 19:15:47 -0300

    ...

--- portfolio-web/src/app/Components/home/home.component.ts
+++ portfolio-web/src/app/Components/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MarqueeComponent } from '../marquee/marquee.component';
 import { TechStackComponent } from '../tech-stack/tech-stack.component';
+import { AsideComponent } from "../aside/aside.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MarqueeComponent,TechStackComponent],
+  imports: [MarqueeComponent, TechStackComponent, AsideComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
